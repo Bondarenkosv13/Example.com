cf34a6dbadc00d87ad71df563cb9690dcb0213aa
Bondarenkosv <bondarenkosv13@gmail.com>
2020-05-16 21:20:25 +0300

    Finish create and show posts.

--- App/Views/Post/show.php
+++ App/Views/Post/show.php
@@ -1,8 +1,14 @@
 <div class="container-md">
-    <h2 class="text-left"><?php echo $title??null; ?></h2>
-    <h6 class="text-muted">Author:<?php echo $name??null;?><br><small>Public date: <?php echo $created_at??null;?></small></h6>
+    <h2 class="text-left"><?php echo $title ?? null; ?></h2>
+    <h6 class="text-muted">Author:<?php echo $name ?? null; ?><br><small>Public
+            date: <?php echo $created_at ?? null; ?></small></h6>
+    <div class="row">
+        <div class="col-md-2"></div>
+        <div class="col-md-6">
+            <img src="/Assets/<?php echo $image;?>" alt="<?php echo $title ?? null; ?>">
+        </div>
+        <div class="col-md-4"></div>
+    </div>
     <br>
-    <img src="../../../Public/Assets/Image/57/1589622830_unnamed.jpg" alt="<?php echo $title??null; ?>" >
-    <br>
-    <p class="text-justify lead" ><?php echo $content??null; ?></p>
+    <p class="text-justify"><?php echo nl2br($content ?? null); ?></p>
 </div>
